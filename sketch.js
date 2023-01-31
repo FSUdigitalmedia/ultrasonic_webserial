8b62453eba1980277c2fdcbdf6543ae8dd9a508c
Rob Duarte <me@robduarte.com>
2023-01-31 11:08:12 -0500

    moved serial funcs to own file; smoothed animation

--- sketch.js
+++ sketch.js
@@ -16,113 +16,48 @@ by Rob Duarte
 
 // variable to hold an instance of the p5.webserial library:
 const serial = new p5.WebSerial();
-// port chooser button:
-let portButton;
-// variable for incoming serial data:
-let inData;
+let inData; // variable for incoming serial data
+
+let w = 0; // width of the bar graph
+const speed = .02; // how quickly the bar graph should grow/shrink
 
 function setup() {
-  createCanvas(400,100);
+  createCanvas(660,100);
   background(255);
   noStroke();
 
-  // check to see if serial is available:
+  // set up all of the functions that will deal with serial stuff
+  // the functions are defined in a separate file (serialfunctions.js)
   if (!navigator.serial) {
     alert("WebSerial is not supported in this browser. Try Chrome or MS Edge.");
   }
-  // check for any ports that are available:
-  serial.getPorts();
-  // if there's no port chosen, choose one:
-  serial.on("noport", makePortButton);
-  // open whatever port is available:
-  serial.on("portavailable", openPort);
-  // handle serial errors:
-  serial.on("requesterror", portError);
-  // handle any incoming serial data:
-  serial.on("data", serialEvent);
-  serial.on("close", makePortButton)
+  serial.getPorts();                    // find serial ports
+  serial.on("noport", makePortButton);  // choose a serial port
+  serial.on("portavailable", openPort); // open the port
+  serial.on("requesterror", portError); // handle serial errors
+  serial.on("data", serialEvent);       // handle incoming serial data
+  serial.on("close", makePortButton);
   // add serial connect/disconnect listeners:
   navigator.serial.addEventListener("connect", portConnect);
   navigator.serial.addEventListener("disconnect", portDisconnect);
 }
 
-// if there's no port selected, 
-// make a port select button appear:
-function makePortButton() {
-  // create and position a port chooser button:
-  portButton = createButton('choose port');
-  portButton.position(10, 10);
-  // give the port button a mousepressed handler:
-  portButton.mousePressed(choosePort);
-}
-
-// make the port selector window appear:
-function choosePort() {
-  if (portButton) portButton.show();
-  serial.requestPort();
-}
-
 function draw() {
-  if (inData >= 2 && inData < 100) {
+  if (inData >= 2 && inData < 200) { // try to keep out weird data
     background(255);
-    if (inData < 10) { 
-      fill("red");
-    } else if (inData < 40) { 
-      fill("orange");
-    } else {
-      fill("green");
-    }
-    let w = inData*3;
-    rect(40, height/2, w, 50);
+    
+    let distance = inData - w; // distance between current value and previous one
+    w = w + (distance*speed);  // how we smooth out the jittery movement of the bar
+    
+    let wScaled = w*3; // scale it up so it's more visible on screen
+    // and change color of the bar based on how big it is on screen...
+    if (wScaled < 30) { fill("red") } 
+    else if (wScaled < 120) { fill("orange") } 
+    else { fill("green") }
+
+    rect(40, height/2, wScaled, 50); 
+
     fill("black");
-    text(inData, w+50, height/2+28);
+    text(inData, w*3+50, height/2+28);
   }
-}
-
-// open the selected port, and make the port 
-// button invisible:
-function openPort() {
-  // wait for the serial.open promise to return,
-  // then call the initiateSerial function
-  serial.open().then(initiateSerial);
-
-  // once the port opens, let the user know:
-  function initiateSerial() {
-    console.log("port open");
-  }
-  // hide the port button once a port is chosen:
-  if (portButton) portButton.hide();
-}
-
-// pop up an alert if there's a port error:
-function portError(err) {
-  alert("Serial port error: " + err);
-}
-// read any incoming data as a string
-// (assumes a newline at the end of it):
-function serialEvent() {
-  // read a string from the serial port:
-  var inString = serial.readLine();
-  // check to see that there's actually a string there:
-  if (inString) {
-    // convert it to a number:
-    inData = Number(inString);
-  }
-}
-
-// try to connect if a new serial port 
-// gets added (i.e. plugged in via USB):
-function portConnect() {
-  console.log("port connected");
-  serial.getPorts();
-}
-
-// if a port is disconnected:
-function portDisconnect() {
-  serial.close();
-  console.log("port disconnected");
-}
-
-function closePort() {
-  serial.close();
 }
